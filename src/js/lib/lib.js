f37e790ee2fb0a1ee27d57ad6c0770e23418f881
Dima Moskalenko <dmtrmsklnk@gmail.com>
2022-08-30 19:27:49 +0300

    add dropdown

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -4,6 +4,7 @@ import './modules/classes';
 import './modules/handler';
 import './modules/attribute';
 import './modules/actions';
-import './modules/effects'
+import './modules/effects';
+import './components/dropdown';
 
 export default $;
